ebae59948f0f7d29740407b3dc02934ef2ea7913
elida vellayoudom <vellayoudom.elida@gmail.com>
2023-12-15 10:26:45 +0100

    adding get msg

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ model = IrisModel()
 # 3. Expose the prediction functionality, make a prediction from the passed
 #    JSON data and return the predicted flower species with the confidence
 
+@app.get('/')
+def get_message():
+    return {
+        'message' : "Hello, stranger, from GitHub actions"
+        }
+
 @app.post('/predict')
 def predict_species(iris: IrisSpecies):
     data = iris.dict()
